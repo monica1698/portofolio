ba26cc7b053910c32dee39cfd3c7a116a783c099
monica1698 <monica.constandachi@gmail.com>
2024-09-19 18:57:51 +0300

    Band name generator update

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("Welcome to band name generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+
+print("Your band name could be", city +" "+ pet)
